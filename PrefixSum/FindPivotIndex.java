0d01a7cb2a2885ddd243bb655c7a308028c80ff4
Your Name <kanyangif@gmail.com>
2025-01-27 20:00:53 +0300

    Finding pivot index

--- PrefixSum/FindPivotIndex.java
+++ PrefixSum/FindPivotIndex.java
@@ -1,6 +1,30 @@
 public class FindPivotIndex {
-    public int pivotIndex(int[] nums) {
+    public int pivotIndex(int[] nums) { 
+       int sumLeft = 0;
+       int totalSum = 0;
+
+       for(int num:nums){
+        totalSum += num;
+
+       }
+       for(int i = 0; i < nums.length; i++){
+        if(sumLeft == totalSum - sumLeft - nums[i]){
+            return i;
+        }
+
+        sumLeft +=nums[i];
+    }
+
+       return -1;
         
+    }
+
+    public static void main(String[] args) {
+        FindPivotIndex myMethod = new FindPivotIndex();
+
+        int []myArray = {1,7,3,6,5,6};
+
+        System.out.println(myMethod.pivotIndex(myArray)); 
     }
     
 }
