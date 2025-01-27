309adba054ebac4efd5788714cf1821d3e0d4b7d
Your Name <kanyangif@gmail.com>
2025-01-27 20:21:54 +0300

    Give difference of two arrays

--- HashMaps/TwoArrayDifference.java
+++ HashMaps/TwoArrayDifference.java
@@ -1,5 +1,21 @@
 package HashMaps;
 
+import java.util.List;
+
 public class TwoArrayDifference {
-    
+    public List <List<Integer>> findDifference(int []nums1, int[]nums2){
+
+        for(int i = 0; i < nums1.length; i++){
+            if(nums2.Contains());
+        }
+
+        
+    }
+    public static void main(String[] args) {
+        TwoArrayDifference myMethod = new TwoArrayDifference();
+        int []num1 = {1,2,3};
+        int []num2 = {2,4,6};
+
+        System.out.println(myMethod.findDifference(num1, num2));
+    }
 }
