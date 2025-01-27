81a162b6a51b88671e890f4a1253db071cc1d4f1
Your Name <kanyangif@gmail.com>
2025-01-27 20:50:48 +0300

    Finding the difference of two arrays

--- HashMaps/TwoArrayDifference.java
+++ HashMaps/TwoArrayDifference.java
@@ -1,16 +1,43 @@
 package HashMaps;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class TwoArrayDifference {
-    public List <List<Integer>> findDifference(int []nums1, int[]nums2){
+   public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+    List<Integer> uniqueInNums1 = new ArrayList<>();
+    List<Integer> uniqueInNums2 = new ArrayList<>();
 
-        for(int i = 0; i < nums1.length; i++){
-            if(nums2.Contains());
-        }
-
-        
+    Set<Integer> set2 = new HashSet<>();
+    for (int num : nums2) {
+        set2.add(num);
     }
+
+    for (int num : nums1) {
+        if (!set2.contains(num)) {
+            uniqueInNums1.add(num);
+        }
+    }
+
+    Set<Integer> set1 = new HashSet<>();
+    for (int num : nums1) {
+        set1.add(num);
+    }
+
+    for (int num : nums2) {
+        if (!set1.contains(num)) {
+            uniqueInNums2.add(num);
+        }
+    }
+
+    List<List<Integer>> result = new ArrayList<>();
+    result.add(uniqueInNums1);
+    result.add(uniqueInNums2);
+
+    return result;
+}
     public static void main(String[] args) {
         TwoArrayDifference myMethod = new TwoArrayDifference();
         int []num1 = {1,2,3};
