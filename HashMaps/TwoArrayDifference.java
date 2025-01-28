a350a0a9a00c14742dd968012d25c9ac24c27944
Your Name <kanyangif@gmail.com>
2025-01-28 19:42:15 +0300

    Find the difference of two arrays

--- HashMaps/TwoArrayDifference.java
+++ HashMaps/TwoArrayDifference.java
@@ -1,48 +1,41 @@
-package HashMaps;
-
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class TwoArrayDifference {
-   public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
-    List<Integer> uniqueInNums1 = new ArrayList<>();
-    List<Integer> uniqueInNums2 = new ArrayList<>();
+    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
 
-    Set<Integer> set2 = new HashSet<>();
-    for (int num : nums2) {
-        set2.add(num);
-    }
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
 
-    for (int num : nums1) {
-        if (!set2.contains(num)) {
-            uniqueInNums1.add(num);
+        for (int num : nums1) {
+            set1.add(num);
         }
-    }
 
-    Set<Integer> set1 = new HashSet<>();
-    for (int num : nums1) {
-        set1.add(num);
-    }
-
-    for (int num : nums2) {
-        if (!set1.contains(num)) {
-            uniqueInNums2.add(num);
+        for (int num : nums2) {
+            set2.add(num);
         }
+
+        Set<Integer> uniqueInNums1 = new HashSet<>(set1);
+        uniqueInNums1.removeAll(set2);
+
+        Set<Integer> uniqueInNums2 = new HashSet<>(set2);
+        uniqueInNums2.removeAll(set1);
+
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(new ArrayList<>(uniqueInNums1));
+        result.add(new ArrayList<>(uniqueInNums2));
+
+        return result;
     }
 
-    List<List<Integer>> result = new ArrayList<>();
-    result.add(uniqueInNums1);
-    result.add(uniqueInNums2);
-
-    return result;
-}
     public static void main(String[] args) {
         TwoArrayDifference myMethod = new TwoArrayDifference();
-        int []num1 = {1,2,3};
-        int []num2 = {2,4,6};
 
-        System.out.println(myMethod.findDifference(num1, num2));
+        int[] nums1 = {1, 2, 3, 3};
+        int[] nums2 = {1, 1, 2, 2};
+
+        System.out.println(myMethod.findDifference(nums1, nums2));
     }
 }
